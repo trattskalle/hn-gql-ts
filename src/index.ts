7f10fada4ef5627eea14cb96535ca09008a3f729
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-04-17 17:22:06 +0200

    Setup apollo server caching

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { ApolloServer } from "apollo-server";
+import responseCachePlugin from "apollo-server-plugin-response-cache";
+
 import { resolvers } from "./hn";
 import * as fs from "fs";
 
@@ -7,8 +9,11 @@ const typeDefs = fs.readFileSync("./schema.graphql", "utf8").toString();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  tracing: true,
+  cacheControl: true,
   introspection: true,
   playground: true,
+  plugins: [responseCachePlugin()],
 });
 
 server.listen(process.env.PORT || 4000).then(({ url }) => {
